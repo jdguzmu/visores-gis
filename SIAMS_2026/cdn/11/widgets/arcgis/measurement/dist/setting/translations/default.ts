f14a3be606b91f6d88102965b4850fc9a01ab000
jdguzmu <jdguzmu@gmail.com>
2026-08-26 11:01:17 +0200

    siams260826

--- SIAMS_2026/cdn/11/widgets/arcgis/measurement/dist/setting/translations/default.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  distance: 'Distance',
-  area: 'Area',
-  atLeastOne: 'At least one tool should be selected',
-  toolbar: 'Toolbar',
-  disableSnapping: 'Disable snapping in 2D map'
-}
